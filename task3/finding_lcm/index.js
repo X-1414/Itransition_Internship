c3541dcfce40e768b54d37cd5f0288f4cc0c436f
X-1414 <khv.uzb14@gmail.com>
2026-06-14 14:52:25 +0500

    modified for floating point overflow

--- task3/finding_lcm/index.js
+++ task3/finding_lcm/index.js
@@ -6,7 +6,7 @@ function isNatural(n){
 }
 
 function gcd(x,y){
-    while(y!==0){
+    while(y!==0n){
         [x,y] = [y,x%y];
     }
     return x;
@@ -17,13 +17,17 @@ function lcm(x,y){
 }
 
 app.get(["/", "/khv_uzb14_gmail_com"], (req, res) => {
-    const x = Number(req.query.x);
-    const y = Number(req.query.y);
+    const x = BigInt(req.query.x);
+    const y = BigInt(req.query.y);
+
+    if (x <= 0n || y <= 0n) {
+        return res.send("NaN");
+    }
 
     if (!isNatural(x) || !isNatural(y)) {
         return res.send("NaN");
     }
-
+    
     const result = lcm(x, y);
     res.type("text/plain").send(result.toString());
 });

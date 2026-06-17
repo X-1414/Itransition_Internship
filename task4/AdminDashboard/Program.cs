880be41d44d7f7afb173cdf2b83ece075c9bf663
X-1414 <khv.uzb14@gmail.com>
2026-06-18 01:25:52 +0500

    fix target framework to net8.0

--- task4/AdminDashboard/Program.cs
+++ task4/AdminDashboard/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddScoped<IEmailService, EmailService>();
 builder.Services.Configure<ForwardedHeadersOptions>(options =>
 {
     options.ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto;
-    options.KnownIPNetworks.Clear();
+    options.KnownNetworks.Clear();
     options.KnownProxies.Clear();
 });
 // auth login
